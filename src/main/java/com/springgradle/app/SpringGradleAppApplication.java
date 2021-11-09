76d16574b880ac44b3cf951dd1259bd9bbad3d84
Vinod <mvinodkumarjava@gmail.com>
2021-11-09 18:42:37 +0530

    First Commit

---
+++ src/main/java/com/springgradle/app/SpringGradleAppApplication.java
@@ -0,0 +1,13 @@
+package com.springgradle.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGradleAppApplication.class, args);
+	}
+
+}

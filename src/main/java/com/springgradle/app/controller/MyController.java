16ade3c26b99ea76df9399afb8f8247ae07509f0
Vmuthav-BRONGA <Vinod_Kumar_Muthavarapu@gap.com>
2021-11-26 17:45:20 +0530

    code copy to mywork branch

--- src/main/java/com/springgradle/app/controller/MyController.java
+++ src/main/java/com/springgradle/app/controller/MyController.java
@@ -1,13 +1,18 @@
 package com.springgradle.app.controller;
 
+import org.apache.logging.log4j.LogManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
+	
+	org.apache.logging.log4j.Logger logger = LogManager.getLogger(MyController.class);
 
 	@GetMapping
 	public String hello() {
+		
+		logger.error("Error occured");
 		return "Hi Gradle";
 	}
 }
